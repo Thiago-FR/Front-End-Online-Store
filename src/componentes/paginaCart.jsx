9a898c5f8aa32d4c69395b8870c53cdbc821365b
douglas oliveira <douglasgama0@gmail.com>
2021-10-27 15:55:45 -0300

    Iniciando req12

--- src/componentes/paginaCart.jsx
+++ src/componentes/paginaCart.jsx
@@ -7,12 +7,14 @@ class paginaCart extends Component {
     const lista = pegarProduto();
     this.state = {
       listaProdutos: lista === null ? [] : [...lista],
+      maxQuantidade: false,
     };
 
     this.removeProduto = this.removeProduto.bind(this);
     this.addItem = this.addItem.bind(this);
     this.removeItem = this.removeItem.bind(this);
     this.pegarListaProdutos = this.pegarListaProdutos.bind(this);
+    this.pegarPrecoTotal = this.pegarPrecoTotal.bind(this);
   }
 
   pegarListaProdutos() {
@@ -26,22 +28,38 @@ class paginaCart extends Component {
   }
 
   removeItem(item) {
-    item.quantidade -= 1;
-    this.setState({}, () => atualizaItem(item));
+    if (item.quantidade > 0) {
+      item.quantidade -= 1;
+      this.setState({}, () => atualizaItem(item));
+    }
+  }
+
+  pegarPrecoTotal() {
+    const { listaProdutos } = this.state;
+    let precoTotal = 0;
+    listaProdutos.forEach((item) => {
+      precoTotal += item.price * item.quantidade;
+    });
+    return precoTotal.toFixed(2);
   }
 
   addItem(item) {
-    item.quantidade += 1;
-    this.setState({}, () => atualizaItem(item));
+    if (item.quantidade < item.available_quantity) {
+      item.quantidade += 1;
+      this.setState({ maxQuantidade: false }, () => atualizaItem(item));
+    } else {
+      this.setState({ maxQuantidade: true });
+    }
   }
 
   render() {
-    const { listaProdutos } = this.state;
+    const { listaProdutos, maxQuantidade } = this.state;
     return (
       <div>
         { listaProdutos.length === 0 && (
           <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
         ) }
+        { maxQuantidade && <p>Quantidade de item superior ao estoque</p> }
         { listaProdutos.map((item, i) => (
           <div key={ `${item.id}-${i}` }>
             <img src={ item.thumbnail } alt="" />
@@ -79,6 +97,10 @@ class paginaCart extends Component {
             </button>
           </div>
         )) }
+        <p>
+          Total R$:
+          { this.pegarPrecoTotal() }
+        </p>
       </div>
     );
   }
