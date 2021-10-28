87fb8046315f71227f976e6a10bbbf75cb113126
Thiago <thiago_ssfr@hotmail.com>
2021-10-28 16:41:39 -0300

    feat: req13

--- src/componentes/CartButton.jsx
+++ src/componentes/CartButton.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class CartButton extends Component {
   render() {
+    const { quantidade } = this.props;
+    console.log('carrinho', quantidade);
     return (
       <div>
-        <Link to="/carrinho" data-testid="shopping-cart-button">Carrinho</Link>
+        <Link to="/carrinho" data-testid="shopping-cart-button">
+          Carrinho
+          <span data-testid="shopping-cart-size">{ quantidade }</span>
+        </Link>
       </div>
     );
   }
 }
+
+CartButton.propTypes = {
+  quantidade: PropTypes.number,
+};
+
+CartButton.defaultProps = {
+  quantidade: 0,
+};
 
 export default CartButton;
--- src/componentes/produtos.jsx
+++ src/componentes/produtos.jsx
@@ -10,7 +10,9 @@ class Produtos extends React.Component {
   }
 
   adicionarProduto(element) {
+    const { atualizaLista } = this.props;
     salvarProduto(element);
+    atualizaLista();
   }
 
   render() {
@@ -58,6 +60,7 @@ class Produtos extends React.Component {
 }
 
 Produtos.propTypes = {
+  atualizaLista: PropTypes.func.isRequired,
   resultado: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
